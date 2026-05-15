5bc10188e45712e681d1f781e58edda021b9fd6c
unknown <pedroalvarenga173@gmail.com>
2026-05-15 19:03:30 -0330

    feat: new methods for the player

--- Entities/PlayerBase/player_class.gd
+++ Entities/PlayerBase/player_class.gd
@@ -1,15 +1,13 @@
 extends Node2D
 class_name Player
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 @export var health = 100
 @export var charName : String
+@export var hurtbox: Area2D
+@export var progressBar: ProgressBar
+
+func _ready() -> void:
+	return
+	
+func DamageTaken(damage):
+	health -= damage
