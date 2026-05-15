d79f9a359ff98ef6f6404a48123b46ef3642bc9b
unknown <pedroalvarenga173@gmail.com>
2026-05-15 19:07:12 -0330

    docs: code comments for better understanding

--- Entities/PlayerBase/player_class.gd
+++ Entities/PlayerBase/player_class.gd
@@ -1,13 +1,19 @@
 extends Node2D
 class_name Player
 
+# Exporta variaveis para o reuso
 @export var health = 100
 @export var charName : String
 @export var hurtbox: Area2D
 @export var progressBar: ProgressBar
 
-func _ready() -> void:
-	return
-	
+# Método de entrada, toda vez q for estanciada a primeira coisa a ser exec
+func _ready():
+	progressBar.max_value = health
+	progressBar.value = health
+
+# Metodo de tomar dano
 func DamageTaken(damage):
 	health -= damage
+	if health < 1:
+		queue_free()
